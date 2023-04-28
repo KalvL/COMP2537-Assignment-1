77133dabf7faba4741d9370081f5c426b1c3d6af
Kalvin <leung.kalvin@gmail.com>
2023-04-27 18:46:08 -0700

    Finished

--- index.js
+++ index.js
@@ -40,6 +40,8 @@ var mongoStore = MongoStore.create({
 	}
 })
 
+var username;
+
 app.use(session({ 
     secret: node_session_secret,
 	store: mongoStore, //default is memory store 
@@ -49,7 +51,26 @@ app.use(session({
 ));
 
 app.get('/', (req,res) => {
-    res.send("<h1>Hello World!</h1>");
+    username = req.session.username;
+    if (!req.session.authenticated) {
+        res.send(`
+        <form action='/signup' method='get'>
+            <button> Sign Up </button>
+            <br>
+            <button type='submit' formaction='/login' formmethod='get'> Login </button>
+        </form>
+        `);
+    } else {
+        res.send(`
+        <form action='/members' method='get'>
+            <div>Hello, ${username} !</div>
+            <br>
+            <button>Go to Members Area</button>
+            <br>
+            <button formaction='/logout' formmethod='get'>Logout</button>
+        </form>
+        `);
+    }
 });
 
 app.get('/nosql-injection', async (req,res) => {
@@ -114,13 +135,17 @@ app.post('/submitEmail', (req,res) => {
 });
 
 
-app.get('/createUser', (req,res) => {
+app.get('/signup', (req,res) => {
     var html = `
     create user
     <form action='/submitUser' method='post'>
-    <input name='username' type='text' placeholder='username'>
-    <input name='password' type='password' placeholder='password'>
-    <button>Submit</button>
+        <input name='name' type='text' placeholder='name'>
+        <br>
+        <input name='email' type='text' placeholder='email'>
+        <br>
+        <input name='password' type='password' placeholder='password'>
+        <br>
+        <button>Submit</button>
     </form>
     `;
     res.send(html);
@@ -131,72 +156,109 @@ app.get('/login', (req,res) => {
     var html = `
     log in
     <form action='/loggingin' method='post'>
-    <input name='username' type='text' placeholder='username'>
-    <input name='password' type='password' placeholder='password'>
-    <button>Submit</button>
+        <input name='email' type='text' placeholder='email'>
+        <input name='password' type='password' placeholder='password'>
+        <button>Submit</button>
     </form>
     `;
     res.send(html);
 });
 
 app.post('/submitUser', async (req,res) => {
-    var username = req.body.username;
+    var username = req.body.name;
+    var email = req.body.email;
     var password = req.body.password;
+    var html = "";
+    let missingField = false;
 
-	const schema = Joi.object(
-		{
-			username: Joi.string().alphanum().max(20).required(),
-			password: Joi.string().max(20).required()
-		});
-	
-	const validationResult = schema.validate({username, password});
-	if (validationResult.error != null) {
-	   console.log(validationResult.error);
-	   res.redirect("/createUser");
-	   return;
-   }
+    if (username && email && password) {
+        const schema = Joi.object(
+            {
+                username: Joi.string().max(20).required(),
+                email: Joi.string().required(),
+                password: Joi.string().max(20).required()
+            });
 
-    var hashedPassword = await bcrypt.hash(password, saltRounds);
-	
-	await userCollection.insertOne({username: username, password: hashedPassword});
-	console.log("Inserted user");
+        const validationResult = schema.validate({ username, email, password });
+        if (validationResult.error != null) {
+            console.log(validationResult.error);
+            res.redirect("/signup");
+            return;
+        }
 
-    var html = "successfully created user";
-    res.send(html);
+        var hashedPassword = await bcrypt.hash(password, saltRounds);
+
+        await userCollection.insertOne({ username: username, email: email, password: hashedPassword });
+        console.log("Inserted user");
+
+        var html = "successfully created user";
+        res.send(html);
+        return;
+    } else {
+        if (!username) {
+            html += "Name is required<br>";
+            missingField = true;
+        }
+        if (!email) {
+            html += "Email is required<br>";
+            missingField = true;
+        }
+        if (!password) {
+            html += "Password is required<br>";
+            missingField = true;
+        }
+        if (missingField) {
+            html += "<br><a href='/signup'>Try Again</a>";
+        }
+        res.send(html);
+        return;
+    }
 });
 
 app.post('/loggingin', async (req,res) => {
+    var email = req.body.email;
     var username = req.body.username;
     var password = req.body.password;
 
-	const schema = Joi.string().max(20).required();
-	const validationResult = schema.validate(username);
+	const schema = Joi.string().max(50).required();
+	const validationResult = schema.validate(email);
 	if (validationResult.error != null) {
 	   console.log(validationResult.error);
 	   res.redirect("/login");
 	   return;
 	}
 
-	const result = await userCollection.find({username: username}).project({username: 1, password: 1, _id: 1}).toArray();
+	const result = await userCollection.find({email: email}).project({username: 1, email: 1, password: 1, _id: 1}).toArray();
+    var html = "";
 
 	console.log(result);
 	if (result.length != 1) {
-		console.log("user not found");
-		res.redirect("/login");
-		return;
+		console.log("email not found");
+		// res.redirect("/login");
+		// return;
+        html += `
+            Invalid email/password combination
+            <br>
+            <a href='/login'>Try Again</a>`;
+        res.send(html);
+        return
 	}
 	if (await bcrypt.compare(password, result[0].password)) {
 		console.log("correct password");
 		req.session.authenticated = true;
-		req.session.username = username;
+		req.session.username = result[0].username;
 		req.session.cookie.maxAge = expireTime;
-
-		res.redirect('/loggedIn');
+		res.redirect('/members');
 		return;
 	}
 	else {
 		console.log("incorrect password");
-		res.redirect("/login");
+		// res.redirect("/login");
+        html += `
+            Invalid email/password combination
+            <br><br>
+            <a href='/login'>Try Again</a>`;
+        res.send(html);
 		return;
 	}
 });
@@ -204,43 +266,57 @@ app.post('/loggingin', async (req,res) => {
 app.get('/loggedin', (req,res) => {
     if (!req.session.authenticated) {
         res.redirect('/login');
+        return;
     }
     var html = `
-    You are logged in!
+    You are logged in! ${req.session.username}
     `;
     res.send(html);
 });
 
 app.get('/logout', (req,res) => {
 	req.session.destroy();
-    var html = `
-    You are logged out.
-    `;
-    res.send(html);
+    // var html = `
+    // You are logged out.
+    // `;
+    // res.send(html);
+    res.redirect('/');
 });
 
 
-app.get('/cat/:id', (req,res) => {
+app.get('/members', (req,res) => {
+    if (req.session.authenticated) {
+        let html = `<p>Hello ${req.session.username}<p><br>`;
+        // var cat = req.params.id;
+        let cat = Math.floor(Math.random() * 3) + 1;
 
-    var cat = req.params.id;
-
-    if (cat == 1) {
-        res.send("Fluffy: <img src='/fluffy.gif' style='width:250px;'>");
+        if (cat == 1) {
+            html += "Fluffy: <img src='/fluffy.gif' style='width:250px;'>";
+        } else if (cat == 2) {
+            html += "Socks: <img src='/socks.gif' style='width:250px;'>";
+        } else if (cat == 3) {
+            html += "NyanCat: <img src='/nyancat.gif' style='width:250px;'>";
+        } else {
+            html += `Invalid cat id: ${cat}`;
+        }
+        html += `<form action='/logout' method='get'><button>Sign out</button>`;
+        res.send(html);
+    } else {
+        res.redirect('/logout');
     }
-    else if (cat == 2) {
-        res.send("Socks: <img src='/socks.gif' style='width:250px;'>");
-    }
-    else {
-        res.send("Invalid cat id: "+cat);
-    }
+    
 });
 
 
 app.use(express.static(__dirname + "/public"));
 
+app.get("/does_not_exist", (req, res) => {
+    res.status(404);
+    res.send(`Page not found - 404 <br><img src='/404.gif' style='width:250px;'>`);
+})
+
 app.get("*", (req,res) => {
-	res.status(404);
-	res.send("Page not found - 404");
+	res.redirect('/does_not_exist');
 })
 
 app.listen(port, () => {
